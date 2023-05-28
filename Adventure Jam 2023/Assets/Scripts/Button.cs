449e64f1cdbd53424a53a483fc9672fa12f50176
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2023-05-28 21:19:13 +0100

    Created a modular button that can call any code

---
+++ Adventure Jam 2023/Assets/Scripts/Button.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    private bool playerNear;
+    private bool buttonActive;
+    public UnityEvent buttonAction;
+
+    [SerializeField] private GameObject ButtonObj;
+    [SerializeField] private GameObject ButtonPressedObj;
+
+    private void Update()
+    {
+        if(playerNear)
+        {
+            if(Input.GetButtonDown("Interact"))
+            {
+                triggerButtonState();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerNear = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerNear = false;
+        }
+    }
+
+    private void triggerButtonState()
+    {
+        if(buttonActive)
+        {
+            buttonActive = false;
+            ButtonPressedObj.SetActive(false);
+            ButtonObj.SetActive(true);
+            buttonAction.Invoke();
+        }
+        else
+        {
+            buttonActive = true;
+            ButtonPressedObj.SetActive(true);
+            ButtonObj.SetActive(false);
+            buttonAction.Invoke();
+        }
+    }
+}
