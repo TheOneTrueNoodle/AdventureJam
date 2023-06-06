5086573d4ab0adf4c673cdfb962344b80d5cec11
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2023-06-06 14:14:32 +0100

    Audio work and importing assets

--- Adventure Jam 2023/Assets/Scripts/AudioManager.cs
+++ Adventure Jam 2023/Assets/Scripts/AudioManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -6,5 +7,24 @@ public class AudioManager : MonoBehaviour
     public AudioMixerGroup music;
     public AudioMixerGroup SFX;
 
+    public Sound[] sounds;
 
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            GameObject soundObj = new GameObject(s.name);
+            s.source = soundObj.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
 }
--- Adventure Jam 2023/Assets/Scripts/Sound.cs
+++ Adventure Jam 2023/Assets/Scripts/Sound.cs
@@ -1,10 +1,15 @@
 using UnityEngine.Audio;
 using UnityEngine;
 
+[System.Serializable]
 public class Sound
 {
+    public string name;
+
     public AudioClip clip;
 
-    public float volume;
-    public float pitch;
+    [Range(0, 1)] public float volume = 1;
+    [Range(0.1f, 3)] public float pitch = 1;
+
+    [HideInInspector] public AudioSource source;
 }
